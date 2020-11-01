d8e6779f694ac25b861127bf5b1672e647fb4a8d
leifb <leif.bernsdorf@gmail.com>
2020-11-01 14:51:44 +0100

    Added env variables

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ async function start() {
     try {
         rrb.Defaults.setDefaults({
             redis: {
-                host: "mhredis",
-                prefix: "mh:"
+                prefix: process.env.REDIS_PREFIX || 'mh:',
+                host: process.env.REDIS_HOST || "mhredis",
+                port: process.env.REDIS_PORT || undefined,
+                db: process.env.REDIS_DB || undefined,
+                password: process.env.REDIS_PASSWORD || undefined
             }
         });
         await permissions.start();
